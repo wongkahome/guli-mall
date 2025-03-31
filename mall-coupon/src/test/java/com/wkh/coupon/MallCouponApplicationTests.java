a4bacd7211037fdf8f2b09d64e41408e1a79a512
wongkahome <2584033104@qq.com>
2025-03-31 11:58:11 +0800

    项目初始结构创建

---
+++ mall-coupon/src/test/java/com/wkh/coupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wkh.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ mall-ware/src/test/java/com/wkh/ware/MallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wkh.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
